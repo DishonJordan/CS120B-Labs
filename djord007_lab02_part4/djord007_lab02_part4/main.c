c0f4f3bb12d62c535ff796b503b4f05f8da4796f
DishonJordan <dishonjordan@hotmail.com>
2019-04-07 11:26:58 -0700

    Finished Part 4

--- djord007_lab02_part4/djord007_lab02_part4/main.c
+++ djord007_lab02_part4/djord007_lab02_part4/main.c
@@ -18,19 +18,27 @@ int main(void)
 		unsigned char weight_b = 0x00;
 		unsigned char weight_c = 0x00;
 		unsigned char total_weight = 0x00;
-		signed char weight_difference = 0x00;
+		signed short weight_difference = 0x00;
+		unsigned char temp_output = 0x00;
 
     /* Replace with your application code */
     while (1) 
     {
-		weight_a = PINA; weight_b = PINB; weight_c = PINC;
+		weight_a = PINA; 
+		weight_b = PINB; 
+		weight_c = PINC;
+
 		total_weight = weight_a + weight_b + weight_c;
+
 		weight_difference = weight_a - weight_c;
-		PORTD = total_weight > 0x8C ? 0x01: 0x00;
-		if(weight_difference > 0x50 || weight_difference < -0x50){
-			PORTD = PORTD | 0x02;
+
+		temp_output = total_weight > 140 ? 0x01: 0x00;
+		if(weight_difference > 80 || weight_difference < -80){
+			temp_output = temp_output | 0x02;
 		}
 
+		PORTD = temp_output;
+		
 
     }
 }
