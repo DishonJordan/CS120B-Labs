6c32f8c0dca39a5eb8e6f5647b68b35b6d815cec
DishonJordan <dishonjordan@hotmail.com>
2019-04-08 22:06:26 -0700

    Added lab3 part 1 template

---
+++ djord007_lab03_part1/djord007_lab03_part1/main.c
@@ -0,0 +1,21 @@
+/*	Partner 1 Name & E-mail: Dishon Jordan djord007@ucr.edu
+ *	Partner 2 Name & E-mail: Travis Nasser tnass001@ucr.edu
+ *	Lab Section: 
+ *	Assignment: Lab 3  Exercise 1 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
