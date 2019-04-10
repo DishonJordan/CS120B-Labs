85f2199086a7b42fd0b0298770f714174effdf84
Dishon Jordan <43739227+DishonJordan@users.noreply.github.com>
2019-04-09 22:14:52 -0700

    Update main.c

--- djord007_lab03_part1/djord007_lab03_part1/main.c
+++ djord007_lab03_part1/djord007_lab03_part1/main.c
@@ -1,21 +1,33 @@
 /*	Partner 1 Name & E-mail: Dishon Jordan djord007@ucr.edu
  *	Partner 2 Name & E-mail: Travis Nasser tnass001@ucr.edu
- *	Lab Section: 
+ *	Lab Section: 025
  *	Assignment: Lab 3  Exercise 1 
  *	Exercise Description: [optional - include for your own benefit]
  *	
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-
 #include <avr/io.h>
 
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char counter = 0;
+	
+	while(1) {
+		counter = 0;
+		for (unsigned char i = 0; i < 8; i++) {
+			if (GetBit(PINA, i) == 1) { counter++; }
+			if (GetBit(PINB, i) == 1) { counter++; }
+		}
+		PORTC = counter;
+	}
+	return 0;
 }
 
