880649e2d3755c45fca7450bccf73187e896a2d2
DishonJordan <dishonjordan@hotmail.com>
2019-04-12 00:40:16 -0700

    Worked on lab04 part 3

--- djord007_lab04_part3/djord007_lab04_part3/main.c
+++ djord007_lab04_part3/djord007_lab04_part3/main.c
@@ -11,80 +11,92 @@
 #include <avr/io.h>
 
 enum Lock_States{ Lock_Start, Lock_Combo1, Lock_Combo2, Lock_Unlock}Current_State;
-
-int main(void)
-{
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-
 	unsigned char lock_status = 0x01;
 	unsigned char x_button = 0x00;
 	unsigned char y_button = 0x00;
 	unsigned char pound_button = 0x00;
 	unsigned char inside_button = 0x00;
+	unsigned char c_state_debug = 0x00;
+
+void LockSM(){
+		switch(Current_State){//Transition Controller
+		case Lock_Start:
+			Current_State =  (pound_button && !x_button && !y_button)? Lock_Combo1 : Lock_Start;
+			break;
+		case Lock_Combo1:
+			if(x_button || y_button){
+				Current_State = Lock_Start;
+			}else if(!pound_button){
+				Current_State = Lock_Combo2;
+			}else{
+				Current_State = Lock_Combo1;
+			}
+
+		break;
+		case Lock_Combo2:
+			if(pound_button || x_button){
+				Current_State = Lock_Start;
+			}else if(y_button){
+				Current_State = Lock_Unlock;
+			}else{
+				Current_State = Lock_Combo2;
+			}
+
+
+		break;
+		case Lock_Unlock:
+			if(inside_button){
+				Current_State = Lock_Start;
+			}else{
+				Current_State = Lock_Unlock;
+			}
+		break;
+		default:
+		break;
+	}
+
+	switch(Current_State){//State Actions Controller
+		case Lock_Start:
+			lock_status = 0x01;
+			c_state_debug = 0x00;
+			break;
+		case Lock_Combo1:
+			lock_status = 0x01;
+			c_state_debug = 0x01;
+			break;
+		case Lock_Combo2:
+			lock_status = 0x01;
+			c_state_debug = 0x02;
+			break;
+		case Lock_Unlock:
+			lock_status = 0x00;
+			c_state_debug = 0x04;
+			break;
+		default:
+			c_state_debug = 0xFF;
+		break;
+	}
+}
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+
+
 	Current_State = Lock_Start;
     /* Replace with your application code */
     while (1) 
     {
 		x_button = PINA & 0x01;
-		y_button = PINA & 0x02 >> 1;
-		pound_button = PINA & 0x04 >> 2;
-		inside_button = PINA & 0x08 >> 3;
-
-		switch(Current_State){//Transition Controller
-			case Lock_Start:
-				Current_State =  (pound_button && !x_button && !y_button)? Lock_Combo1 : Lock_Start;
-				break;
-			case Lock_Combo1:
-				if(x_button || y_button){
-					Current_State = Lock_Start;
-				}else if(!pound_button){
-					Current_State = Lock_Combo2;
-				}else{
-					Current_State = Lock_Combo1;
-				}
-
-			break;
-			case Lock_Combo2:
-				if(pound_button || x_button){
-					Current_State = Lock_Start;
-				}else if(y_button){
-					Current_State = Lock_Unlock;
-				}else{
-					Current_State = Lock_Combo2;
-				}
-
-
-			break;
-			case Lock_Unlock:
-				if(inside_button){
-					Current_State = Lock_Start;
-				}else{
-					Current_State = Lock_Unlock;
-				}
-			break;
-			default:
-			break;
-		}
-
-		switch(Current_State){//State Actions Controller
-			case Lock_Start:
-				lock_status = 0x01;
-				break;
-			case Lock_Combo1:
-				lock_status = 0x01;
-				break;
-			case Lock_Combo2:
-				lock_status = 0x01;
-				break;
-			case Lock_Unlock:
-				lock_status = 0x00;
-				break;
-			default:
-			break;
-		}
-
+		y_button = (PINA & 0x02) >> 1;
+		pound_button = (PINA & 0x04) >> 2;
+		inside_button = (PINA & 0x80) >> 7;
+		LockSM();
 		PORTB = lock_status;
+		PORTC = c_state_debug;
     }
 }
 
