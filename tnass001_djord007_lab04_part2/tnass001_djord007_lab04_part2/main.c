a9c0cd2641024342204758ffa87db9fc821849a4
Travis Nasser <nassertravis@gmail.com>
2019-04-12 20:36:46 -0700

    lab 04 part 2 Finished

--- tnass001_djord007_lab04_part2/tnass001_djord007_lab04_part2/main.c
+++ tnass001_djord007_lab04_part2/tnass001_djord007_lab04_part2/main.c
@@ -1,100 +1,138 @@
-/*
- * tnass001_djord007_lab04_part2.c
- *
- * Created: 4/11/2019 10:38:27 PM
- * Author : nasse
- */ 
-
+/*	Partner 1 Name & E-mail: Dishon Jordan djord007@ucr.edu
+*	Partner 2 Name & E-mail: Travis Nasser tnass001@ucr.edu
+*	Lab Section: 025
+*	Assignment: Lab 4  Exercise 2
+*	Exercise Description:
+*
+*	I acknowledge all content contained herein, excluding template or example
+*	code, is my own original work.
+*/
 #include <avr/io.h>
 
-enum States {Init, Increment, Decrement, Reset} State;
+enum States {start, init, inc, dec, w1, w2, w3, res_w, res_c, } state;
 
 void tick() {
-	switch(State) {			
-		case Init:
-			if ((PINA & 0x03) == 0x02) {
-				State = Decrement;
+	switch(state) {	
+		case start:
+			state = init;
+			break;
+			
+		case init:
+			if ((PINA & 0x03) == 0x01) {
+				state = inc;
 			}
-			else if ((PINA & 0x03) == 0x01) {
-				State = Increment;
+			else if((PINA & 0x03) == 0x02) {
+				state = dec;
 			}
 			else if ((PINA & 0x03) == 0x03) {
-				State = Reset;
+				state = res_c;
 			}
 			break;
 			
-		case Increment:
-			if ((PINA & 0x03) == 0x02) {
-				State = Decrement;
+		case inc:
+			state = w1;
+			break;
+			
+		case w1:
+			if((PINA & 0x03) == 0x01) {
+				state = w1;
 			}
-			else if ((PINA & 0x03) == 0x01) {
-				State = Increment;
+			else if((PINA & 0x03) == 0x00) {
+				state = res_w;
 			}
 			else if((PINA & 0x03) == 0x03) {
-				State = Reset;
-			}
-			break;
-			
-		case Decrement:
-			if ((PINA & 0x03) == 0x02) {
-				State = Decrement;
-			}
-			else if ((PINA & 0x03) == 0x01) {
-				State = Increment;
-			}
-			else if((PINA & 0x03) == 0x03) {
-				State = Reset;
-			}
-			break;
-			
-		case Reset:
-			if ((PINA & 0x03) == 0x03) {
-				State = Reset;
+				state = res_c;
 			}
 			else if ((PINA & 0x03) == 0x02) {
-				State = Decrement;
-			}
-			else if ((PINA & 0x03) == 0x01) {
-				State = Increment;
+				state = dec;
 			}
 			break;
 			
-		default:
-			break;
-	}
-	
-	switch(State) {
-		case Init:
-			PORTC = 0x07;
-			break;
-			
-		case Increment: 
-			if (PORTC > 9) {
-				break;
-			}
-			PORTC += 1;
+		case dec:
+			state = w2;
 			break;
 		
-		case Decrement:
-			if (PORTC <= 0) {
-				break;
+		case w2:
+			if((PINA & 0x03) == 0x02) {
+				state = w2;
 			}
-			PORTC -= 1;
+			else if((PINA & 0x03) == 0x00) {
+				state = res_w;
+			}
+			else if((PINA & 0x03) == 0x03) {
+				state = res_c;
+			}
+			else if ((PINA & 0x03) == 0x01) {
+				state = inc;
+			}
 			break;
 			
-		case Reset:
+		case res_w:
+			if ((PINA & 0x03) == 0x01) {
+				state = inc;
+			}
+			else if ((PINA & 0x03) == 0x02) {
+				state = dec;
+			}
+			else if ((PINA & 0x03) == 0x03) {
+				state = res_c;
+			}
+			break;
+			
+		case res_c:
+			state = w3;
+			break;
+			
+		case w3:
+			state = w3;
+			if ((PINA & 0x03) == 0x00) {
+				state = init;
+			}
+			break;
+			
+		default:
+			state = init;
+	}
+	
+	switch(state) {
+		case init:
+			break;
+			
+		case inc:
+			if (PORTC < 9) { PORTC += 1; }
+			break;
+		
+		case w1:
+			break;
+			
+		case dec:
+			if (PORTC > 0) { PORTC -= 1; }
+			break;
+			
+		case w2:
+			break;
+			
+		case res_w:
+			break;
+			
+		case res_c:
 			PORTC = 0x00;
+			break;
+			
+		case w3:
+			break;
 			
 		default:
 			break;
 	}
+			
 }
 
 int main(void)
 {
    DDRA = 0x00; PORTA = 0xFF;
    DDRC = 0xFF; PORTC = 0x07;
-   State = Init;
+   state = init;
 
    while(1) {
 	   tick();
