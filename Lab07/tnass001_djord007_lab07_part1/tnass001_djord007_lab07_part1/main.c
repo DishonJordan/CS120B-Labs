6e4ddb03e42282c7218299460aa9852252932cf3
Travis Nasser <nassertravis@gmail.com>
2019-04-26 20:12:32 -0700

    lab 7 part 2 including synchSM

--- Lab07/tnass001_djord007_lab07_part1/tnass001_djord007_lab07_part1/main.c
+++ Lab07/tnass001_djord007_lab07_part1/tnass001_djord007_lab07_part1/main.c
@@ -1,8 +1,75 @@
+/*	Partner 1 Name & E-mail: Dishon Jordan djord007@ucr.edu
+*	Partner 2 Name & E-mail: Travis Nasser tnass001@ucr.edu
+*	Lab Section: 025
+*	Assignment: Lab 5 Exercise 2
+*	Exercise Description:
+*
+*	I acknowledge all content contained herein, excluding template or example
+*	code, is my own original work.
+*/
 #include <avr/io.h>
+#include <avr/interrupt.h>	
 #include "io.c"
 
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
+	// bit2bit1bit0=011: pre-scaler /64
+	// 00001011: 0x0B
+	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
+	// Thus, TCNT1 register will count at 125,000 ticks/s
+
+	// AVR output compare register OCR1A.
+	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
+	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
+	// So when TCNT1 register equals 125,
+	// 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1=0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+
+void TimerOff() {
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
 enum States {init, wait, inc, dec, reset, incWait, decWait, resWait} state;
-unsigned char tmpVal;
+unsigned char cValue = 0;
 
 void tick() {
 	unsigned char tmpA = ~PINA;
@@ -77,44 +144,47 @@ void tick() {
 	
 	switch(state) {
 		case init:
-			break;
+		cValue = 0;
+		//LCD_ClearScreen();
+		LCD_WriteData(cValue + '0');
+		break;
 		
 		case wait:
-			break;
+		break;
 		
 		case inc:
-			LCD_ClearScreen();
-			LCD_WriteData(tmpVal + '0');
-			if (tmpVal < 9) {
-				++tmpVal;
+		if (cValue < 9) { 
+			cValue += 1; 
+		//	LCD_ClearScreen();
+			LCD_WriteData(cValue + '0');
 			}
-			break;
+		break;
 		
 		case dec:
-			LCD_ClearScreen();
-			LCD_WriteData(tmpVal + '0');
-			if (tmpVal > 0) {
-				--tmpVal;
+		if (cValue > 0) {
+			cValue -= 1; 
+		//	LCD_ClearScreen();
+			LCD_WriteData(cValue + '0');
 			}
-			break;
+		break;
 		
 		case reset:
-			tmpVal = 0x00;
-			LCD_ClearScreen();
-			LCD_WriteData(tmpVal + '0');
-			break;
+			cValue = 0;
+		//	LCD_ClearScreen();
+			LCD_WriteData(cValue + '0');
+		break;
 		
 		case incWait:
-			break;
+		break;
 		
 		case decWait:
-			break;
+		break;
 		
 		case resWait:
-			break;
+		break;
 		
 		default:
-			break;
+		break;
 	}
 }
 
@@ -123,12 +193,14 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
 	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
-	tmpVal = 0;
 	
 	state = init;
 	LCD_init();
-	
+
 	while(1) {
-		tick();	
+		tick();
+		while(!TimerFlag)
+			TimerFlag = 0;
 	}
 }
+
