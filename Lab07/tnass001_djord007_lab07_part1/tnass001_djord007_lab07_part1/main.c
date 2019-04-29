8c718336e3d1cd811170cf2059b5d3b31be6f89b
Travis Nasser <nassertravis@gmail.com>
2019-04-29 09:13:09 -0700

    Lab 7 part 1 Finished

--- Lab07/tnass001_djord007_lab07_part1/tnass001_djord007_lab07_part1/main.c
+++ Lab07/tnass001_djord007_lab07_part1/tnass001_djord007_lab07_part1/main.c
@@ -2,16 +2,53 @@
 *	Partner 2 Name & E-mail: Travis Nasser tnass001@ucr.edu
 *	Lab Section: 025
 *	Assignment: Lab 7 Exercise 1
-*	Exercise Description:
+*	Exercise Description:	
 *
 *	I acknowledge all content contained herein, excluding template or example
 *	code, is my own original work.
 */
 #include <avr/io.h>	
 #include "io.c"
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
 
 enum States {init, wait, inc, dec, reset, incWait, decWait, resWait} state;
-unsigned char cValue = 0;
+unsigned char cValue = 0, timer = 0x00;
 
 void tick() {
 	unsigned char tmpA = ~PINA;
@@ -92,22 +129,23 @@ void tick() {
 		break;
 		
 		case wait:
+		timer = 0;
 		break;
 		
 		case inc:
-		if (cValue < 9) { 
-			cValue += 1; 
-			LCD_Cursor(1);
-			LCD_WriteData(cValue + '0');
+			if (cValue < 9) {
+				cValue += 1;
+				LCD_Cursor(1);
+				LCD_WriteData(cValue + '0');
 			}
 		break;
 		
 		case dec:
 		if (cValue > 0) {
-			cValue -= 1; 
+			cValue -= 1;
 			LCD_Cursor(1);
 			LCD_WriteData(cValue + '0');
-			}
+		}
 		break;
 		
 		case reset:
@@ -117,12 +155,27 @@ void tick() {
 		break;
 		
 		case incWait:
+		timer++;
+		if (cValue < 9 && (timer % 10 == 0)) {
+			cValue += 1;
+			LCD_Cursor(1);
+			LCD_WriteData(cValue + '0');
+			
+		}
 		break;
 		
 		case decWait:
+		timer++;
+			if (cValue > 0 && (timer % 10 == 0)) {
+				cValue -= 1;
+				LCD_Cursor(1);
+				LCD_WriteData(cValue + '0');
+				
+			}
 		break;
 		
 		case resWait:
+		timer = 0;
 		break;
 		
 		default:
@@ -136,6 +189,9 @@ int main(void)
 	DDRC = 0xFF; PORTC = 0x00; // LCD data lines
 	DDRD = 0xFF; PORTD = 0x00; // LCD control lines
 	
+	TimerSet(100);
+	TimerOn();
+	
 	LCD_init();
 	LCD_Cursor(1);
 	
@@ -143,6 +199,9 @@ int main(void)
 	LCD_DisplayString(1, "0");
 	while(1) {
 		tick();
+		
+		while(!TimerFlag);
+			TimerFlag = 0;
 	}
 }
 
